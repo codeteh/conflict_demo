b09bd42b8c4786c9bcea9269f734ce1520826d37
codeteh <101430666+codeteh@users.noreply.github.com>
2023-12-14 16:35:04 -0800

    add status

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,10 @@
-def hello_world():
-    print(f'Hello World!')
+Update the content of helloworld.py in user2's conflict_demo/ folder as follows:
+def hello_world(state):
+    print(f'Hello World {state}!')
 
 
 def main():
-    hello_world()
+    hello_world('New York')
 
 
 main()
